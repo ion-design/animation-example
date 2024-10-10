b61b28dbf302caf7e0b085e5d96a141b24b38734
Samraaj Bath <samraaj100@gmail.com>
2024-10-10 13:27:17 -0700

    Animate component: src/app/settings/page.tsx

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,15 +3,21 @@
 import SettingsPage from "@/components/SettingsCard";
 import SubscribeToContinueCard from "@/components/Subscribe";
 import SideNavigation from "@/components/ion/SideNavigation";
-
+import { motion } from 'framer-motion';
 
 function TestPage() {
   return (
-      <div className="w-screen h-screen flex">
-        <SideNavigation />
-        <div className="bg-container flex-1 h-full w-full flex justify-center items-center gap-5 p-10">
+    <div className="w-screen h-screen flex">
+      <SideNavigation />
+      <div className="bg-container flex-1 h-full w-full flex justify-center items-center gap-5 p-10">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4 }}
+        >
           <SettingsPage />
-        </div>
+        </motion.div>
+      </div>
     </div>
   );
 }
