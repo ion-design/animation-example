eeb47f02518d2fdb8fabd19510933371aafb5768
Samraaj Bath <samraaj100@gmail.com>
2024-10-10 13:27:02 -0700

    Animate component: src/app/banking/page.tsx

--- src/app/banking/page.tsx
+++ src/app/banking/page.tsx
@@ -1,17 +1,24 @@
 'use client';
 
+import React from 'react';
+import { motion } from 'framer-motion';
 import SubscribeToContinueCard from "@/components/Subscribe";
 import SideNavigation from "@/components/ion/SideNavigation";
 
-
 function TestPage() {
   return (
-      <div className="w-screen h-screen flex">
-        <SideNavigation />
-        <div className="bg-container flex-1 h-full w-full flex justify-center items-center gap-5 p-10">
-          <SubscribeToContinueCard />
-        </div>
-    </div>
+    <motion.div 
+      className="w-screen h-screen flex"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <SideNavigation />
+      <div className="bg-container flex-1 h-full w-full flex justify-center items-center gap-5 p-10">
+        <SubscribeToContinueCard />
+      </div>
+    </motion.div>
   );
 }
+
 export default TestPage;
